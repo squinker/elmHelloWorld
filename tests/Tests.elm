14d17e45d04440f94e7301f294b4c8d055b64f9b
William Bellamy <william.bellamy@itv.co.uk>
2017-06-21 13:50:53 +0100

    Add some test tests

---
+++ tests/Tests.elm
@@ -0,0 +1,20 @@
+module Tests exposing (..)
+
+import Test exposing (..)
+import Expect
+import Fuzz exposing (list, int, tuple, string)
+import String
+
+
+all : Test
+all =
+    describe "Sample Test Suite"
+        [ describe "Unit test examples"
+            [ test "Addition" <|
+                \() ->
+                    Expect.equal (3 + 7) 10
+            , test "String.left" <|
+                \() ->
+                    Expect.equal "a" (String.left 1 "abcdefg")
+            ]
+        ]
